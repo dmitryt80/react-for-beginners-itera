b4cc65299af4c58d7d5c51446fbef0ba064140be
Dmitry Tartsaiev <dmitantar80@gmail.com>
2022-04-29 13:08:01 +0300

    added new code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  <h1>Hello World!</h1>
+      <h1>Name: Dmitry Tartsaiev</h1>
+      <p className="biography">Short biography: I studyed  frontend development at IT school "GOIT".
+      I'm a switcher and i like developing and learn somthing new all the time. 
+      My hobby is read books, play chess, write code </p>
+      <p className="my-contacts">Public contacts:
+    <span className="contact-item">phone: +38(096)788-39-64</span>  
+    <span className="contact-item">e-mail: dmitantar80@gmail.com</span>
+      </p>
     </div>
   );
 }
